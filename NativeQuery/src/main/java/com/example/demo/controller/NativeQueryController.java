a6cc182625971258258ae213be1f9f0b3d637cd7
kiki202181 <usagi211neko@yahoo.co.jp>
2021-11-19 07:29:55 +0900

    Controller変更

--- NativeQuery/src/main/java/com/example/demo/controller/NativeQueryController.java
+++ NativeQuery/src/main/java/com/example/demo/controller/NativeQueryController.java
@@ -1,5 +1,29 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+//import com.example.demo.entity.User;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
 public class NativeQueryController {
 
+	
+	@GetMapping("/")
+	public String view(Model model) {
+		//全取得
+		return "View";
+	}
+	
+	@PostMapping("/search")
+	public String edit(Model model) {
+
+		return "Edit";
+	}
+	
 }
