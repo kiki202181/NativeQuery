a23b2d6843dee01236695e7ecae59811cc779e2b
kiki202181 <usagi211neko@yahoo.co.jp>
2021-11-21 11:09:05 +0900

    MySQLから取得するクエリを追加

--- NativeQuery/src/main/java/com/example/demo/controller/NativeQueryController.java
+++ NativeQuery/src/main/java/com/example/demo/controller/NativeQueryController.java
@@ -1,9 +1,15 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
 
 //import com.example.demo.entity.User;
 
@@ -13,10 +19,17 @@ import lombok.RequiredArgsConstructor;
 @Controller
 public class NativeQueryController {
 
+	@Autowired
+	UserRepository userRepository;
+	List<User> UserList;
 	
 	@GetMapping("/")
 	public String view(Model model) {
 		//全取得
+		UserList=userRepository.findAllUser();
+		model.addAttribute("UserList",UserList);
+		
+		
 		return "View";
 	}
 	
--- NativeQuery/src/main/java/com/example/demo/repository/UserRepository.java
+++ NativeQuery/src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,10 +1,15 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.demo.entity.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
-	
+	 @Query(value = "SELECT * FROM user_tb" , nativeQuery = true)
+	   List<User> findAllUser();
 
 }
